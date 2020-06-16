4a2320761bbe205b98650844834d982b50c02d25
Daggy1234 <arnav.jindal7@gmail.com>
2020-06-16 20:31:05 +0530

    Delete config.py

--- app/config.py
+++
@@ -1,8 +0,0 @@
-
-redditcred = {
-    "client_secret":"	TIjY0UIWjP7V5wbFkjW4hxBcEA8",
-    "user_agent":"getmemeapi",
-    "client_id":'HnP5vVpMlc5lrQ',
-    "username":"dagbotthebot",
-    "password":"ARnav123#"
-}
